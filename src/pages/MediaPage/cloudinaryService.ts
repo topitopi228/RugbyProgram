e082d0d4bca8f94f7a46cd1f4dd9fa88d6623f22
topitopi228 <serhiymajor2701smsm@gmail.com>
2026-06-13 13:57:04 +0300

    fix

--- src/pages/MediaPage/cloudinaryService.ts
+++ src/pages/MediaPage/cloudinaryService.ts
@@ -127,7 +127,6 @@ const STATIC_IMAGES: Record<string, string[]> = {
         'v1780652048/888_yqd6qk',
         'v1780652048/%D0%B7%D0%B7%D0%B7%D0%B7%D0%B7_t5fecg',
         'v1780652046/%D1%83%D1%83%D1%83%D1%83_ndujhw',
-        'v1780652045/IMG_9079_fo09ex',
         'v1780652045/8_wycrnq',
         'v1780652043/2_ygwqfk',
         'v1780652043/5_f9yvre',
